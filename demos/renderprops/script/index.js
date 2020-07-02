1f1212877064ad95313ca8ac930be2d68152f873
Naotho Machida <naotho.machida@gmail.com>
2020-07-02 20:58:59 -0300

    width and height 100% don't take everything on the screen

--- demos/renderprops/script/index.js
+++ demos/renderprops/script/index.js
@@ -43,8 +43,8 @@ export default class App extends React.Component {
     return (
       <div
         style={{
-          width: '100%',
-          height: '100%',
+          width: window.innerWidth,
+          height: window.innerHeight,
           overflow: 'hidden',
           background: 'aquamarine',
           padding: 10,
